beb395b29ce394c53031f1aa45ff77b57d923e22
Norbe <55793789+norbertas156@users.noreply.github.com>
2024-10-28 23:08:43 +0200

    Add files via upload

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN_number;  // Incorrect naming convention
+        public int pgeCnt;          // Unclear and short variable name
+        public bool Checked;        // Ambiguous name for a boolean property
+
+        public Book(string t, string a, string isbn, int pageCnt, bool chkd) // Confusing parameter names
+        {
+            Title = t;
+            Author = a;
+            ISBN_number = isbn;
+            pgeCnt = pageCnt;
+            Checked = chkd;
+        }
+    }
+}
---
+++ LibraryManagementSystem/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Member
+    {
+        public int memberId;
+        public string FullName;
+        public string email_address;
+
+        public Member(int id, string name, string email)
+        {
+            memberId = id;
+            FullName = name;
+            email_address = email;
+        }
+    }
+}
