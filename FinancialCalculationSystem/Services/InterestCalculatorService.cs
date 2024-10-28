df705d88fb6ad27b49d3ac8035a97ae45694096f
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2024-10-28 22:44:35 +0200

    Add files via upload

---
+++ FinancialCalculationSystem/Services/InterestCalculatorService.cs
@@ -0,0 +1,18 @@
+﻿using FinancialCalculationSystem.Models;
+using System;
+
+namespace FinancialCalculationSystem.Services
+{
+    public class InterestCalculatorService
+    {
+        public double CalculateSimpleInterest(double principal, float rate, int years)
+        {
+            return principal * rate * years / 100;
+        }
+
+        public double CalcCompIntrst(double principal, float rate, int years)
+        {
+            return principal * Math.Pow((1 + rate / 100), years) - principal;
+        }
+    }
+}
